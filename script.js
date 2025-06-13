05bc4c44ad335813a600d55453e11ab4664519a0
Bindhu2u <himabindhupathipaka383@gmail.com>
2025-06-13 10:17:21 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Simple JavaScript</title>
+</head>
+<body>
+
+  <h1>JavaScript Example</h1>
+  <button onclick="sayHello()">Click Me</button>
+
+  <script>
+    function sayHello() {
+      alert("Hello from JavaScript!");
+    }
+  </script>
+
+</body>
+</html>
